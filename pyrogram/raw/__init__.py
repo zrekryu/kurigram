8e162a3439eb811ebd9e5878339440ac29aa8dfe
Zrekryu <145272593+zrekryu@users.noreply.github.com>
2026-02-20 08:43:33 +0530

    Comment out dynamic import logic in __init__.py
    Comment out the dynamic import logic for objects.

--- pyrogram/raw/__init__.py
+++ pyrogram/raw/__init__.py
@@ -21,6 +21,6 @@ from importlib import import_module
 from . import types, functions, base, core
 from .all import objects
 
-for k, v in objects.items():
-    path, name = v.rsplit(".", 1)
-    objects[k] = getattr(import_module(path), name)
+#for k, v in objects.items():
+#    path, name = v.rsplit(".", 1)
+#    objects[k] = getattr(import_module(path), name)
