5a6e3ce37e970d799b37d5defb3e196161365040
Zrekryu <zrekryu@gmail.com>
2026-06-04 16:31:30 +0530

    Add return type annotation to propagation methods

--- pyrogram/types/update.py
+++ pyrogram/types/update.py
@@ -16,12 +16,14 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Never
+
 import pyrogram
 
 
 class Update:
-    def stop_propagation(self):
+    def stop_propagation(self) -> Never:
         raise pyrogram.StopPropagation
 
-    def continue_propagation(self):
+    def continue_propagation(self) -> Never:
         raise pyrogram.ContinuePropagation
